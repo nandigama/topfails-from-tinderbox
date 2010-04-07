095ed3c3b9b6475fcc0ce38fe554328cb911eee8
Murali Nandigama <murali.nandigama@gmail.com>
2010-04-06 22:11:58 -0700

    first batch of code

---
+++ unittest-logs/unittestweb/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('unittestweb.viewer.views',
+    (r'^$', 'index'),
+    (r'^/$', 'index'),
+    (r'^trees$', 'trees'),
+    (r'^trees/(?P<tree>.+)$', 'tree'),
+    (r'^changesets$', 'changesets'),
+    (r'^changesets/(?P<changeset>[a-f0-9]+)$', 'changeset'),
+    (r'^tests$', 'tests'),
+    (r'^test$', 'test'),
+    (r'^timeline$', 'timeline'),
+    (r'^topfails$', 'topfails'),
+)
